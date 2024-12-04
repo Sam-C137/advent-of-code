12355ad52c5293c15cd73c0c6c1a74a51b373caa
Sam-C137 <sam.ackah@amalitech.com>
2024-12-04 15:16:49 +0000

    step: 2024 - day 4

---
+++ 2024/4/x-mas.ts
@@ -0,0 +1,39 @@
+async function main() {
+    const file = Bun.file("input.txt");
+    const text = await file.text();
+    const lines = text
+        .split(/\n/)
+        .map((l) => l.trim())
+        .map((str) => str.split(""));
+
+    return count(lines);
+}
+
+function count(grid: string[][]): number {
+    let count = 0;
+
+    for (let i = 1; i < grid.length - 1; i++) {
+        for (let j = 1; j < grid[i].length - 1; j++) {
+            if (grid[i][j] !== "A") continue;
+
+            const first = grid[i - 1][j - 1] + grid[i][j] + grid[i + 1][j + 1];
+            const second = grid[i - 1][j + 1] + grid[i][j] + grid[i + 1][j - 1];
+
+            if (
+                ["MAS", "SAM"].includes(first) &&
+                ["MAS", "SAM"].includes(second)
+            ) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+try {
+    const result = await main();
+    console.log(`The total number of X shaped MAS occurrences are ${result}`);
+} catch (e) {
+    console.error(e);
+}
---
+++ 2024/4/xmas.ts
@@ -0,0 +1,62 @@
+async function main() {
+    const file = Bun.file("input.txt");
+    const text = await file.text();
+    const lines = text
+        .split(/\n/)
+        .map((l) => l.trim())
+        .map((str) => str.split(""));
+
+    return count(lines);
+}
+
+function count(grid: string[][]): number {
+    const word = "XMAS";
+    const directions = [
+        [0, 1],
+        [1, 0],
+        [1, 1],
+        [1, -1],
+    ];
+
+    const reversed = word.split("").reverse().join("");
+    let count = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            for (const [dx, dy] of directions) {
+                let sequence = "";
+
+                const end_x = i + (word.length - 1) * dx;
+                const end_y = j + (word.length - 1) * dy;
+
+                if (
+                    end_x < 0 ||
+                    end_x >= grid.length ||
+                    end_y < 0 ||
+                    end_y >= grid[i].length
+                ) {
+                    continue;
+                }
+
+                for (let k = 0; k < word.length; k++) {
+                    const x = i + k * dx;
+                    const y = j + k * dy;
+                    sequence += grid[x][y];
+                }
+
+                if (sequence === word || sequence === reversed) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+try {
+    const result = await main();
+    console.log(`The total number of XMAS occurrences are ${result}`);
+} catch (e) {
+    console.error(e);
+}
