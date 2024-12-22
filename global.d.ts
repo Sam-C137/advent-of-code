189933b4769552e7070468957ac11230b0e31dd4
Sam-C137 <sam.ackah@amalitech.com>
2024-12-22 12:10:04 +0000

    step: 2024 - day 21

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,9 @@
 declare interface String {
     fromTuple(): number[];
+    deserialize<T>(): T;
 }
 
 declare interface Array<T> {
     toTuple(): string;
+    serialize(): string;
 }
--- utils.ts
+++ utils.ts
@@ -23,4 +23,40 @@ export const is_not_empty = <T>(arr: T[]): arr is NonEmptyArray<T> =>
 export const is_out_of_bounds = <T>(r: number, c: number, grid: T[][]) =>
     r < 0 || r >= grid.length || c < 0 || c >= grid[0].length;
 
+Array.prototype.serialize = function () {
+    return JSON.stringify(this);
+};
+
+String.prototype.deserialize = function <T>() {
+    return JSON.parse(this as string) as T;
+};
+
+export const cartesian_product = (...args: Array<any>[]) =>
+    args.reduce((a, b) => a.flatMap((d) => b.map((e) => [d, e].flat())));
+
+cartesian_product.lazy = function* (...args: Array<any>[]): Generator<any[]> {
+    if (args.length === 0) {
+        yield [];
+        return;
+    }
+    const [first, ...rest] = args;
+    for (const item of first) {
+        for (const prod of cartesian_product.lazy(...rest)) {
+            yield [item, ...prod];
+        }
+    }
+};
+
+export const zip = (...args: (Array<any> | string)[]) => {
+    const min = Math.min(...args.map((arr) => arr.length));
+    return new Array(min).fill(null).map((_, i) => args.map((arr) => arr[i]));
+};
+
+zip.lazy = function* (...args: (Array<any> | string)[]): Generator<any[]> {
+    const min = Math.min(...args.map((arr) => arr.length));
+    for (let i = 0; i < min; i++) {
+        yield args.map((arr) => arr[i]);
+    }
+};
+
 export * from "./ds.ts";
